1a20bcaa1ad3f71a014ad90f02bc76b3784e4c73
Emmanuel Eshun <emmanuel.eshun@amalitech.org>
2022-01-13 10:28:17 +0000

    first commit

---
+++ initials.py
@@ -0,0 +1,9 @@
+# Emmanuel Eshun
+# fun fact: I was defeated by 25 goals in a single fifa match
+print("EEEEE  EEEEE")
+print("E      E")
+print("E      E")
+print("EEE    EEE")
+print("E      E")
+print("E      E")
+print("EEEEE  EEEEE")
